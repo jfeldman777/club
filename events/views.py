703810fa0934bb56c32102a3d9eaebdfd2221362
jfeldman777 <jfeldman777@gmail.com>
2022-12-29 18:34:30 +0200

    lesson 2end

--- events/views.py
+++ events/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.def
 def home(request,year,month):
     name = 'Joe'
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month);
+    month_number = int(month_number)
+    #create calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime('%I:%M:%p') #:@S')
+    #convert mon from str to num
     return render(request,'home.html',{
         "name": name,
         "year": year,
         "month": month,
+        "month_number":month_number,
+        "cal": cal,
+        "current_year":current_year,
+        "time":time,
     })
